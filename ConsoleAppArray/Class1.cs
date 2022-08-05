82d6518b376ffdf6cb1ac443d1e1301de496aef0
A-Olawumi <adedirefausat@gmail.com>
2022-08-05 11:46:49 +0100

    Add project files.

---
+++ ConsoleAppArray/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ConsoleAppArray
+{
+    internal class Class1
+    {
+        public int[] SortedArray(int[] Ulist)
+        {
+            int min = Ulist[0];
+            for (int k = Ulist.Length; k >= 0; k--)
+            {
+                for (int i = 1; i < k; i++)
+                {
+
+                    int a = Ulist[i - 1];
+                    int b = Ulist[i];
+                    if (a < b)
+
+                    {
+
+                        Ulist[i] = a;
+                        Ulist[i - 1] = b;
+                    }
+
+                }
+
+            }
+            return Ulist;
+        }
+    }
+}     
---
+++ ConsoleAppArray/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleAppArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] Ulist = { 43, 56, 67, 78, 89, 23, 12, 34, 56, 78, 19, 29, 35, 57, 32};
+            Console.WriteLine("The Ulist is []{0}", string.Join(",", Ulist));
+            Class1 sortedList = new Class1();
+        int[] output = sortedList.SortedArray(Ulist);
+        Console.WriteLine(" The sorted list is [{0}]",string.Join(",",output));
+        }
+    }
+}
